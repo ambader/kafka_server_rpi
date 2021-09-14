d52303c9940229e719c818dc5c491e1480a33d19
Alexander Bader <a.bader@kree.info>
2021-09-14 08:07:19 +0200

    Update init_kafka.sh

--- scripts/init_kafka.sh
+++ scripts/init_kafka.sh
@@ -12,6 +12,7 @@ mv kafka_2.13-2.7.0 kafka
 
 wget https://raw.githubusercontent.com/ambader/kafka_server_rpi/main/scripts/zookeeper.service -O /etc/systemd/system/zookeeper.service
 wget https://raw.githubusercontent.com/ambader/kafka_server_rpi/main/scripts/kafka.service -O /etc/systemd/system/kafka.service
+wget https://raw.githubusercontent.com/ambader/kafka_server_rpi/main/scripts/py_kafka.service -O /etc/systemd/system/py_kafka.service
 
 wget https://raw.githubusercontent.com/ambader/kafka_server_rpi/main/scripts/start_kafka.sh -O /home/python/start_kafka.sh
 wget https://raw.githubusercontent.com/ambader/kafka_server_rpi/main/scripts/button.py -O /home/python/button.py
